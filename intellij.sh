0ec554b14e7c4100076de11273fe0fac1b9d2e0b
btilford <ben@tilford.info>
2011-07-16 14:30:29 -0600

    Working on intellij setup script

--- intellij.sh
+++ intellij.sh
@@ -18,10 +18,10 @@ cd /apps/idea
 wget "$url"
 mkdir ~/apps/idea/tmp
 tar xzf "$dl" -C ~/apps/idea/tmp
-rm "$dl"
+#rm "$dl"
 ls ~/apps/idea/tmp
 ls ~/apps/idea/tmp | sed -n 's/idea-IU-\([0-9]\{,3\}\.[0-9]\{,3\}\)/mv "&" "~/apps/idea/idea-$version"/p' | sh
-rm -Rf ~/apps/idea/tmp
+#rm -Rf ~/apps/idea/tmp
 ln -s "~/apps/idea/idea-$version" /apps/idea/latest
 
 sudo update-alternatives --install /usr/bin/idea idea "/apps/idea/latest/bin/idea.sh" 1
