f88bba0bd1910457c3534afd193f5c085186a50f
Dominik Danter <dominik@foop.at>
2013-03-06 06:15:21 +0100

    removed maven confusion

--- play.sh
+++ play.sh
@@ -12,7 +12,7 @@ fi
 dl="play-$version.zip"
 url="http://downloads.typesafe.com/play/$version/play-$version.zip"
 
-echo "Setting up maven $version from $url"
+echo "Setting up play $version from $url"
 sudo mkdir -p /opt/play
 cd /opt/play
 sudo wget "$url"
@@ -20,5 +20,5 @@ sudo unzip "$dl"
 sudo rm "$dl"
 sudo ln -s "/opt/play/play-$version" /opt/play/latest
 
-sudo update-alternatives --install /usr/bin/play play "/opt/maven/latest/play" 1
+sudo update-alternatives --install /usr/bin/play play "/opt/ply/latest/play" 1
 sudo update-alternatives --install /usr/bin/play play "/opt/play/play-$version/play" 2
